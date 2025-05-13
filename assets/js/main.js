e2a7beed55d1994c1df0ec3bd23523e2b1cc7a9d
Your Name <your-email@example.com>
2025-05-13 21:17:23 +0530

    commit "Site complated"

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,5 +72,22 @@ const scrollActive = () => {
     })
 }
 window.addEventListener('scroll', scrollActive)
-/*=============== SCROLL REVEAL ANIMATION ===============*/
 
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+});
+
+sr.reveal(`.home__data, .popular__swiper, .footer__container, .footer__info`);
+sr.reveal(`.home__board`, { delay: 700, distance: '100px', origin: 'right' });
+sr.reveal(`.home__pizza`, { delay: 1400, distance: '100px', origin: 'bottom', rotate: { z: -90 } });
+sr.reveal(`.home__ingredient`, { delay: 2000, interval: 100 });
+
+sr.reveal(`.about__data, .recipe__list, .contact__data`, { origin: 'right' });
+sr.reveal(`.about__img, .recipe__img, .contact__image`, { origin: 'left' });
+
+sr.reveal(`.products__card`, { interval: 100 });
